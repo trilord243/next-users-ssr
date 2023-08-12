495ccfc7aae030ba1c0e1e39af3fab4e9563c255
Felipe Escalona <escalonaf12@gmail.com>
2023-08-11 22:47:57 -0400

    agregando ssr :sparkles:

---
+++ app/about/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet fugiat
+        delectus molestiae nam deserunt totam ut tempore, aspernatur, ullam
+        suscipit non quisquam aut ipsum pariatur magnam sint porro officiis,
+        beatae veniam est. Quidem iste consectetur quae, accusantium molestias,
+        omnis nesciunt nobis sed error at eius! Alias sint, eligendi
+        perspiciatis in sequi, aut commodi dolor praesentium quas earum magnam
+        officiis, consequatur totam labore nemo deserunt quia vel saepe.
+        Accusantium ea ut fugit ipsa voluptatum nobis quaerat blanditiis
+        recusandae veniam rerum nostrum dignissimos laudantium quidem laborum
+        maiores tempora maxime, incidunt aut labore reiciendis. Ut corrupti,
+        aperiam nobis deserunt dicta ipsa ipsam velit?
+      </p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ app/services/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function ServicePage() {
+  return (
+    <>
+      <h1>ServicePage</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis
+        commodi, perspiciatis iusto accusantium labore officia expedita. Nihil
+        eos nesciunt cum atque molestias neque consequatur at ipsum temporibus
+        pariatur eius tenetur possimus voluptatibus, dignissimos ducimus ullam
+        commodi sequi officia architecto debitis nisi et. Fuga delectus
+        dignissimos quod tempore tempora. Debitis, magni.
+      </p>
+    </>
+  );
+}
+
+export default ServicePage;
